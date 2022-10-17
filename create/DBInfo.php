a966128c94dab45d9f5912b5024b494d18047bf9
uma-modoki <115713389+uma-modoki@users.noreply.github.com>
2022-10-17 16:44:08 +0900

    Create DBInfo.php

---
+++ create/DBInfo.php
@@ -0,0 +1,10 @@
+<?php
+
+	// DB接続情報を扱うクラス
+	class DBInfo{
+		
+		// クラス内定数
+		const DSN = 'mysql:dbname=umamodoki_mathtest;host=mysql1.php.xdomain.ne.jp;port=3306';
+		const USER = 'umamodoki_root';
+		const PASSWORD = 'rootpass';
+	}
